dd8a51284a42e594685a93185b9796a4937c38ce
HelloWorld2302 <adrar.mateobentoglio@gmail.com>
2023-12-05 15:58:43 +0100

    Modification du JS HTML

--- script/modif_profil.js
+++ script/modif_profil.js
@@ -3,12 +3,14 @@ const inputFirstname = document.querySelector('#firstname');
 const inputEmail = document.querySelector('#email');
 const inputAvatar = document.querySelector('#avatar');
 
+let user;
+
 async function getUser() {
 
     let response = await fetch('https://blabladrar.adrardev.fr/user/id/2');
 
     if (response.ok) {
-        let user = await response.json();
+        user = await response.json();
         
         inputName.value = user["name"];
         inputFirstname.value = user["firstname"];
@@ -22,7 +24,12 @@ async function getUser() {
 
 getUser()
 
-
+function updateUserInfo() {
+    user["name"] = inputName.value;
+    user["firstname"] = inputFirstname.value;
+    user["email"] = inputEmail.value;
+    console.log(user);
+}
 
 // Récupération User - CarPool
 // const retrievedUserJSON = localStorage.getItem('User_CarPool');
