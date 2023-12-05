09fb530d2fdafa9a3648d2367060e1b6817d1cf0
Alexandre F. <100766825+SoulBurn-AlexF@users.noreply.github.com>
2023-12-05 16:23:22 +0100

    Phase 3 - Testing et Alerts - En cours

--- script/createUser.js
+++ script/createUser.js
@@ -5,7 +5,22 @@ async function inscription(InsData){
             method: 'POST',
             body: JSON.stringify(InsData),
         });
+        const result = await response.json();
+        if(result.error == "le compte existe déjà"){
+            alert("Une compte avec ce email existe déjà");
+        } else {
+            alert("Ton Compte Utilisateur à été crée!");
+        }
+    } catch (error){
+        console.log(error);
+    };
+};
 
+async function test(){
+    try{
+        const response = await fetch('https://blabladrar.adrardev.fr/user/all',{
+            method: 'GET',
+        });
         const result = await response.json();
         console.log(result);
     } catch (error){
@@ -32,60 +47,29 @@ class User {
         this.name = name;
         this.email = email;
         this.password = password;
-    }
+    };
 };
 
-let submit = document.getElementById("inscription");
-let lname = document.getElementById('name');
-let firstname = document.getElementById('firstname');
-let email = document.getElementById('email');
-let password = document.getElementById('password');
+let submit = document.querySelector('#submit');
+let lname = document.querySelector('#name');
+let firstname = document.querySelector('#firstname');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
 
 submit.addEventListener('submit', (e) => {
-    
-    console.log(e);
-
     e.preventDefault();
-
-    if((lname.value != null || lname.value != "")&&
-       (firstname.value != null || firstname.value != "")&&
-       (email.value != null || email.value != "")&&
-       (password.value != null || password.value != "")){
-
+    if((lname.value != null || lname.value != "") && (firstname.value != null || firstname.value != "") && (email.value != null || email.value != "") && (password.value != null || password.value != "")){
         //Creation Objet et Ajout des Données
         let newUser = new User(lname.value, firstname.value, email.value, password.value);
-
         // Cote verification
         let verifUser = newUser; // verification(newUser);
-
         //Appele a la function inscription
         if(verifUser != false){
             inscription(verifUser);
         };
-
+        test();
     } else {
-
         console.log("Vous doit remplir tout les champs");
-
     };
 });
 
-
-async function test(){
-    try{
-        const response = await fetch('https://blabladrar.adrardev.fr/user/all',{
-            method: 'GET',
-        });
-
-        const result = await response.json();
-
-        console.log(result);
-
-    } catch (error){
-
-        console.error(error);
-
-    };
-};
-
-test();
