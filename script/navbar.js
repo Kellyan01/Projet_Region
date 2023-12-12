5f3fb3fc34b4f13f7ba83c74f27d3cd5453ce236
Lilldn <lilianldn@icloud.com>
2023-12-12 14:20:23 +0100

    Modification annonces et navbar

--- script/navbar.js
+++ script/navbar.js
@@ -5,6 +5,7 @@ if (userData == null) {
     document.querySelector("#liBoutonDeconnexion").style.display = "none";
     document.querySelector("#liProfil").style.display = "none";
     document.querySelector("#liMesAnnonces").style.display = "none";
+    document.querySelector("#liCreerAnnonce").style.display = "none";
 } else {
     document.querySelector("#liConnexion").style.display = "none";
     document.querySelector("#liInscription").style.display = "none";
